e4a317a0aca752bf4d8a743860809ddccd49bc7d
Holmes Ayala <holmesayala9@gmail.com>
2026-01-22 21:11:11 -0500

    feat: chapter 6 from 6.02 to 6.09

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,18 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+
+    if n <= 1 {
+        return n;
+    }
+
+    let mut numbers: Vec<u32> = vec![0, 1];
+
+    for i in 2..=n as usize {
+        numbers.push(numbers[i - 1] + numbers[i - 2]);
+    }
+
+    numbers[numbers.len() - 1]
 }
 
 #[cfg(test)]
