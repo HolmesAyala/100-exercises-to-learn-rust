f2927afbc2fe8b07cac6fb14982f4f265a59458d
Holmes Ayala <holmesayala9@gmail.com>
2026-01-26 21:26:15 -0500

    feat: chapter 8 from 8.00 to 8.03

--- exercises/08_futures/01_async_fn/src/lib.rs
+++ exercises/08_futures/01_async_fn/src/lib.rs
@@ -1,4 +1,7 @@
-use tokio::net::TcpListener;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+};
 
 // TODO: write an echo server that accepts incoming TCP connections and
 //  echoes the received data back to the client.
@@ -11,7 +14,15 @@ use tokio::net::TcpListener;
 // - `tokio::net::TcpStream::split` to obtain a reader and a writer from the socket
 // - `tokio::io::copy` to copy data from the reader to the writer
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    loop {
+        let (mut tcp_stream, _) = listener.accept().await?;
+        let (mut reader, mut writer) = tcp_stream.split();
+
+        let mut request: Vec<u8> = Vec::new();
+
+        reader.read_to_end(&mut request).await?;
+        writer.write_all(request.as_slice()).await?;
+    }
 }
 
 #[cfg(test)]
